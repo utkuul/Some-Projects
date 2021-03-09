537ac1324373050aa1a7d63f3ceb8fccca202ad0
Utku Çelebi <60467436+utkuul@users.noreply.github.com>
2021-03-09 12:25:17 +0300

    Update Telegram_message_sender_from Python.py

--- Telegram_message_sender_from Python.py
+++ Telegram_message_sender_from Python.py
@@ -13,13 +13,10 @@ vakaSayisi=bilgi[gunSayisi-1]["confirmed"]
 olumSayisi=bilgi[gunSayisi-1]["deaths"]
 iyilesmeSayisi=bilgi[gunSayisi-1]["recovered"]
 
-#mesaj="{} tarihinde \nToplam Vaka sayısı:{}\nToplam Ölüm Sayısı:{}\nToplam İyileşme Sayısı:{}\nSağlıklı Günler Dileriz...".format(tarih,vakaSayisi,olumSayisi,iyilesmeSayisi)
-mesaj = "Şerefsizlik yapmayalım"
+mesaj="{} tarihinde \nToplam Vaka sayısı:{}\nToplam Ölüm Sayısı:{}\nToplam İyileşme Sayısı:{}\nSağlıklı Günler Dileriz...".format(tarih,vakaSayisi,olumSayisi,iyilesmeSayisi)
 print(mesaj)
 
-#requests.post(url = 'https://api.telegram.org/bot1354993750:AAG39h-7rITmd5sefMwNYnAZwNTrDfVltAU/sendMessage',data={'chat_id':1296834804,'text':mesaj}).json() #utku
+#requests.post(url = 'https://api.telegram.org/bot1354993750:AAG39h-7rITmd5sefMwNYnAZwNTrDfVltAU/sendMessage',data={'chat_id':1296834804,'text':mesaj}).json() # me
 
-requests.post(url = 'https://api.telegram.org/bot1354993750:AAG39h-7rITmd5sefMwNYnAZwNTrDfVltAU/sendMessage',data={'chat_id':1173837667,'text':mesaj}).json()
 
-#requests.post(url = 'https://api.telegram.org/bot1354993750:AAG39h-7rITmd5sefMwNYnAZwNTrDfVltAU/sendMessage',data={'chat_id':1248496141,'text':mesaj}).json()
 
